96fb00e23e01fd8831bfe09a11f21de87d0c220d
Christopher Oldfield <coldfield@salesforce.com>
2017-12-23 16:35:25 +1100

    Add new Xcode project

---
+++ 8080-emulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  8080-emulator
+//
+//  Created by Christopher Oldfield on 12/23/17.
+//  Copyright © 2017 humantree. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
